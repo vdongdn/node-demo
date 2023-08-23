2fa5c8a4738dd000d52f95b8f3ec8e4286cef34c
dongnv <dongnv.dng@vnpt.vn>
2023-08-23 15:19:49 +0700

    sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = require('express-handlebars')
+const path = require('path')
+const engine = require('express-handlebars')
 const app = express()
 const port = 3000
 
+//static resources
+app.use(express.static(path.join(__dirname, 'public')))
 //log
 app.use(morgan('combined'))
 
 //tempalte engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine( {
+  extname : ".hbs"
+}
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+console.log(__dirname);
 
-app.get('/start', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
